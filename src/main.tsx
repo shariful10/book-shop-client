c84c575f8aae67ef850aae765fd370842ca89eac
Md. Shariful Islam <freelancershariful10@gmail.com>
2025-01-25 17:28:06 +0600

    Refector

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import { store } from "./redux/store.ts";
 import router from "./routes/routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+		<Toaster
+			position="bottom-right"
+			toastOptions={{ style: { background: "#000", color: "#fff" } }}
+		/>
 	</StrictMode>
 );
