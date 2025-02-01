299443588bbf515d6fbc1ba442af305c14ca52fc
Md. Shariful Islam <freelancershariful10@gmail.com>
2025-02-01 14:43:54 +0600

    Refector

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,3 @@
-const nodeEnv = import.meta.env.NODE_ENV;
-
-export const apiUrl =
-	nodeEnv === "production"
-		? import.meta.env.VITE_API_URL
-		: import.meta.env.VITE_API_LOCAL_URL;
+// import.meta.env.VITE_API_LOCAL_URL
+// import.meta.env.VITE_API_URL
+export const apiUrl = import.meta.env.VITE_API_URL;
